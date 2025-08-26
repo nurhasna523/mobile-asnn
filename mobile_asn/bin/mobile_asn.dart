d64c2857d57f558b31a50a4c5b4ef5e69536872f
Nurhasna Majid <nurhasna23majid@gmail.com>
2025-08-26 13:15:18 +0900

    Project Asn

---
+++ mobile_asn/bin/mobile_asn.dart
@@ -0,0 +1,5 @@
+import 'package:mobile_asn/mobile_asn.dart' as mobile_asn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mobile_asn.calculate()}!');
+}
---
+++ mobile_asn/test/mobile_asn_test.dart
@@ -0,0 +1,8 @@
+import 'package:mobile_asn/mobile_asn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
